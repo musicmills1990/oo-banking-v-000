6f1e4dc46656be487b0e9ff3df5b9e590880503b
Marc Mills <mdmills1990@gmail.com>
2018-09-11 20:55:27 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,10 @@
 class Transfer
-  # your code here
+
+attr_accessor :sender, :receiver, :status, :transfer_amount
+
+def initalize
+  @sender = sender
+  @receiver = receiver
+  @status = "pending"
+  @transfer_amount = transfer_amount
 end
