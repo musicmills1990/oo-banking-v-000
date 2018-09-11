2076c95fd451bf6d6dd91eb3ee66f2dcbd3f63c3
Marc Mills <mdmills1990@gmail.com>
2018-09-11 21:57:18 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,11 +29,11 @@ end
 
 
 def reverse_transfer
-  if @status = "complete"
-  @sender.balance += @amount
-  @receiver.balance -= @amount
-  @status = "reversed"
-end
+    if @status = "complete"
+    @sender.balance += @amount
+    @receiver.balance -= @amount
+    @status = "reversed"
+  end
 end
 
 end
