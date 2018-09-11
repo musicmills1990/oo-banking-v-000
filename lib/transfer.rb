d4fac6648626a6cd319e6091cbac7997f97ff8b9
Marc Mills <mdmills1990@gmail.com>
2018-09-11 21:48:36 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,7 +25,8 @@ def execute_transaction
     @receiver.balance += @amount
     @status = "complete"
   else
-    @status = "Transaction rejected. Please check your account balance."
+    @status = "rejected"
+    "Transaction rejected. Please check your account balance."
   end
 end
 
