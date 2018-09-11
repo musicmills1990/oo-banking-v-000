69d9c697a257683e5066575d5f85ba7e038b9945
Marc Mills <mdmills1990@gmail.com>
2018-09-11 20:57:16 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,7 +2,7 @@ class Transfer
 
 attr_accessor :sender, :receiver, :status, :transfer_amount
 
-def initialize(sender, reciever, transwer_amount)
+def initialize(sender, receiver, transwer_amount)
   @sender = sender
   @receiver = receiver
   @status = "pending"
